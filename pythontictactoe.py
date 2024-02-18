d26dc580e9f4c2d36efbfde57a674584c5cd991e
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2024-02-18 09:55:14 +0000

    minor changes

--- pythontictactoe.py
+++ pythontictactoe.py
@@ -12,14 +12,17 @@ game = [[0, 0, 0],
 #         game[x][y] = 1
 # player1_moves()
 
-def game_board(player=0, row=0, column=0, just_display=False):
-        print("   1  2  3")
-
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+        print("   0  1  2")
         if not just_display:
-                game[row][column] = player
-        for count, row in enumerate(game):
+                game_map[row][column] = player
+        for count, row in enumerate(game_map):
                 print(count, row)
 
-game_board(just_display=True)
+        return game_map
+
+game = game_board(game, just_display=True)
+game = game_board(game, player=1, row=2, column=1)
+
 
 
