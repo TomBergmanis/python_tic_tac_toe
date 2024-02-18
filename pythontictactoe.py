5fc433118e654bc2e747aaf6cdd76fa88efd7b6a
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2024-02-18 10:06:00 +0000

    error handling

--- pythontictactoe.py
+++ pythontictactoe.py
@@ -5,22 +5,17 @@ game = [[0, 0, 0],
         [0, 0, 0], 
         [0, 0, 0], ]
 
-# def player1_moves():
-#         row, column  = input("Please enter your coordinates: ").split()
-#         row = int(row)
-#         column = int(column)
-#         game[x][y] = 1
-# player1_moves()
-
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
-        print("   0  1  2")
-        if not just_display:
-                game_map[row][column] = player
-        for count, row in enumerate(game_map):
-                print(count, row)
+        try:
+                print("   0  1  2")
+                if not just_display:
+                        game_map[row][column] = player
+                for count, row in enumerate(game_map):
+                        print(count, row)
 
-        return game_map
-
+                return game_map
+        except IndexError as e:
+                print("Error: make sure you input row/ column as 0, 1 or 2?", e)
 game = game_board(game, just_display=True)
 game = game_board(game, player=1, row=2, column=1)
 
