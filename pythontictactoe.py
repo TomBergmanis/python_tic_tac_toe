c0452fdf9cb65bac739ca5230e7dc37733a245eb
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2024-02-19 08:10:53 +0000

    added comments to the code

--- pythontictactoe.py
+++ pythontictactoe.py
@@ -43,10 +43,13 @@ def win(current_game):
         return False 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
+
         try:
+                # Stops the users from playing over eachother
                 if game_map[row][column] != 0:
                         print("This position is taken! Chose another")
                         return game_map, False 
+                # Creating a dynamic board size, the user can specify any board size
                 print("   "+"  ".join([str(i) for i in range(len(game_map))]))
                 if not just_display:
                         game_map[row][column] = player
@@ -54,7 +57,6 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
                         print(count, row)
                 return game_map, True 
 
-                return game_map
         except IndexError as e:
                 print("Error: make sure you input row/ column as 0, 1 or 2?", e)
                 return game_map, False
@@ -68,19 +70,20 @@ while play:
         game_size = int(input("What size game of tic tac toe? "))
         game = [[0 for i in range(game_size)] for i in range(game_size)]
         game_won = False
-        player_choice = itertools.cycle([1, 2])
+        player_cycle = itertools.cycle([1, 2]) # Cycles between player 1 and 2 / alternates between the two
         game, _ = game_board(game, just_display=True)
         while not game_won:
-                current_player = next(player_choice)
-                print(f"Current Player: {current_player}")
+                current_player = next(player_cycle)
+                print(f"Current Player: {current_player}") # Shows the current player
                 played = False
-
                 while not played:
+                        # Takes user input for the coordinates for the game. 
                         column_choice = int(input("What column do you want to play? (0, 1, 2): "))
                         row_choice = int(input("What row do you want to play? (0, 1, 2): "))
                         game, played = game_board(game, current_player, row_choice, column_choice)
 
                 if win(game):
+                        # After the game ends or a player wins, the user can choose to play again or quit.
                         game_won = True
                         again = input("The game is over, would you like to play again? (yes/ no): ")
                         if again.lower() == "yes":
